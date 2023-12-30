c08b4abf9f00286273a06e64e902056330b82e37
Ricardo Rojas <rojasricor@gmail.com>
2023-12-30 17:30:12 -0500

    Added execution helper

---
+++ start.sh
@@ -0,0 +1,29 @@
+# start.sh
+
+#!/bin/bash
+
+# Function to display help
+show_help() {
+    echo "Usage: ./start.sh [options] [file]"
+    echo "Options:"
+    echo "  --help       Display this help message"
+    echo "  [file]       Path to the C code file to compile and run (default: get_started/hola.c)"
+    exit 0
+}
+
+# Default route param
+route="get_started/hola.c"
+
+# Check for --help option
+if [ "$1" == "--help" ]; then
+    show_help
+fi
+
+# Check if a parameter is provided
+if [ ! -z "$1" ]; then
+    # If a parameter is provided, use it
+    route="$1"
+fi
+
+# Compile and run the C code with the parameter
+cc "$route" && ./a.out
